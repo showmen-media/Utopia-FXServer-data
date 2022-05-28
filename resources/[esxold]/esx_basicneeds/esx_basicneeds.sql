bbd0da29a7cbb5e845cac5ed2265db4a344fb3a4
Philip Psychias <filippos@psychias.gr>
2022-05-28 21:09:50 +0000

    Removed backup ESX folder

--- resources/[esxold]/esx_basicneeds/esx_basicneeds.sql
+++
@@ -1,6 +0,0 @@
-USE `es_extended`;
-
-INSERT INTO `items` (`name`, `label`, `weight`) VALUES
-	('bread', 'Pain', 1),
-	('water', 'Eau', 1)
-;
--- resources/[esxold]/esx_inventoryhud/html/locales/nl.js
+++
@@ -1,5 +0,0 @@
-var invLocale = new Object();
-invLocale.dropItem = "LAten vallen";
-invLocale.useItem = "Gebruiken";
-invLocale.giveItem = "Geven";
-invLocale.secondInventoryNotAvailable = "Secundaire inventaris is niet beschikbaar.";
