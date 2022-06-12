57639e85560d9f875387988a921b8464e2614bfc
Filippos Psychias <filippos@psychias.gr>
2022-06-12 13:58:02 +0300

    lsia-tower

---
+++ resources/[custom]/[mapmods]/lsia-tower/__resource.lua
@@ -0,0 +1,6 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+this_is_a_map 'yes'
+
+-- https://gta5mod.net/gta-5-mods/maps/control-tower/
+-- https://forum.cfx.re/t/how-to-load-mapeditor-xmls-into-your-server-using-ymaps/42347
